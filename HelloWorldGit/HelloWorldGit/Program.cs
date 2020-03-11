0a75a29719def1f9bf751b3351b09252e0083233
valentyn-petriv <51762577+valentyn-petriv@users.noreply.github.com>
2020-03-11 11:19:54 +0200

    Add files via upload

---
+++ HelloWorldGit/HelloWorldGit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть ваше iм'я\n>>>");
+            String name = Console.ReadLine();
+            Console.WriteLine("Привет " + name + ", поточна дата та час:  " + DateTime.Now.ToString());
+        }
+    }
+}
+
+
